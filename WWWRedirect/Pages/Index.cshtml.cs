22696acc091ccd92c21a1dab4d5ea29cd98fad0c
Adam Stirtan <adamstirtan@gmail.com>
2023-06-24 21:31:14 -0400

    initial commit

---
+++ WWWRedirect/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WWWRedirect.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        string url = "https://adamstirtan.notion.site/adamstirtan/Adam-Stirtan-s-Blog-e3066ed9611f44c2887403ff3680af7d";
+
+        return Redirect(url);
+    }
+}
+
